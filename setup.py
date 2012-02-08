e748571723b395b1c468e5a31e68ba74c84ef609
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-02-08 20:24:58 +0200

    file deletend and dependencies updated

--- setup.py
+++ setup.py
@@ -1,47 +1,44 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
+
 import os
 
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-
-version = read('collective', 'folderlogo', 'version.txt')[:-1]
 
 long_description = (
     open("README.txt").read() + "\n" +
     open(os.path.join("docs", "INSTALL.txt")).read() + "\n" +
     open(os.path.join("docs", "HISTORY.txt")).read() + "\n" +
     open(os.path.join("docs", "CREDITS.txt")).read()
-    )
+)
 
-setup(name='collective.folderlogo',
-      version=version,
-      description="Easily add and change logos in different folders for Plone.",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+
+setup(
+    name='collective.folderlogo',
+    version='0.5',
+    description="Easily add and change logos in different folders for Plone.",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
-        ],
-      keywords='',
-      author='Taito Horiuchi',
-      author_email='taito.horiuchi@gmail.com',
-      url='http://pypi.python.org/pypi/collective.folderlogo',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['collective'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
+    ],
+    keywords='',
+    author='Taito Horiuchi',
+    author_email='taito.horiuchi@gmail.com',
+    url='https://github.com/taito/collective.folderlogo',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['collective'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
 
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
-      )
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)

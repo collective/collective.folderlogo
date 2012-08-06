ebf22123b9e06a524da00237e7055ad917b30e98
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-08-06 22:00:07 +0300

    Release 1.0

--- collective/folderlogo/docs/HISTORY.rst
+++ collective/folderlogo/docs/HISTORY.rst
@@ -1,18 +1,16 @@
 Changelog
 ---------
 
-1.0 (2012-08-XX)
+1.0 (2012-08-06)
 ================
 
 - Configuration moved from properties to registry records. [taito]
 - Dropped support for Python-2.6. [taito]
 
-
 0.4.1 (2011-04-21)
 ==================
 
 - Browser layer added. [taito]
-
 
 0.4.0 (2011-04-15)
 ==================
--- setup.py
+++ setup.py
@@ -17,14 +17,13 @@ long_description = (
 
 setup(
     name='collective.folderlogo',
-    version='0.5',
+    version='1.0',
     description="Easily add and change logos in different folders for Plone.",
     long_description=long_description,
     # Get more strings from
     # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Framework :: Plone",
-        "Framework :: Plone :: 4.1",
         "Framework :: Plone :: 4.2",
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
